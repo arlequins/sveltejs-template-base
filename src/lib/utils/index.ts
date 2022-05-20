1bfa9a4fc8a08ffef45f254f62bbc50f3bf892b3
wonho an <wonho.an@monstar-lab.com>
2022-05-20 12:25:37 +0900

    feat: add error page

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,5 @@
+import { base } from '$app/paths';
+
+export const setPath = (path: string) => {
+  return `${base}${path}`;
+};
---
+++ src/routes/__error.svelte
@@ -0,0 +1,40 @@
+<script lang="ts" context="module">
+  import { StatusCode } from '$lib/constants';
+
+  export const load = ({ error, status }: { error: {
+    message: string,
+    name: string,
+    stack: string,
+  }; status: number }) => {
+    if (Object.hasOwnProperty.call(StatusCode, status)) {
+      return {
+        props: {
+          title: StatusCode[status],
+          error,
+        },
+      };
+    }
+    return {
+      props: {
+        title: `${status}`,
+        error,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  import { Card } from 'svelte-chota';
+  export let title: string;
+</script>
+
+<Card>
+  <h4 slot="header">{title}</h4>
+</Card>
+
+<style>
+  h4 {
+    text-align: center;
+    font-weight: bold;
+  }
+</style>
